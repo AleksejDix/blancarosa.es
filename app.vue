32c5a355d5178fe46ef6d22c796db1ec83de7637
Aleksej Dix <git@aleksejdix.com>
2023-06-17 22:36:30 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-white">
+    <div class="text-pink-600 font-bold">BlancaRosa.es</div>
+    <div>
+      <h1
+        class="text-[256px] font-bold stroke text-white font-serif whitespace-nowrap text-center"
+      >
+        Blanca Rosa
+      </h1>
+      <img src="/img/port.jpg" alt="Santa Pola" />
+
+      <!-- <NuxtWelcome /> -->
+    </div>
+  </div>
+</template>
+
+<style>
+.stroke {
+  -webkit-text-stroke: 2px hotpink;
+  text-stroke: 2px hotpink;
+}
+</style>
